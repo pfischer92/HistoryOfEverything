dbf6bf011247f417115192bc4804dafc3362c1a9
Patrick <mailtopatrick@gmx.net>
2020-06-19 17:53:22 +0200

    prepare for mac os support

---
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/patrick/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/patrick/AndroidStudioProjects/HistoryOfEverything/app"
+export "FLUTTER_TARGET=/Users/patrick/AndroidStudioProjects/HistoryOfEverything/app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/patrick/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=19"
+export "TRACK_WIDGET_CREATION=true"
